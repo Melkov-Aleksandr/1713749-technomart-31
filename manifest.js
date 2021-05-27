90267c1f5ca809fc45b27d9151b1283e1c26e690
Melkov-Aleksandr <83242445+Melkov-Aleksandr@users.noreply.github.com>
2021-05-27 18:17:58 +0500

    f

---
+++ manifest.js
@@ -0,0 +1,6 @@
+{
+    "icons": [
+      { "src": "Photo/fav192.png", "type": "image/png", "sizes": "192x192" },
+      { "src": "Photo/fav512.png", "type": "image/png", "sizes": "512x512" }
+    ]
+}
